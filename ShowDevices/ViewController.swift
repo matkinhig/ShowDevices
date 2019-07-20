8bb51bc8d94237f4aff96514ec006afb3083b635
matkinhig <matkinhig@gmail.com>
2019-07-19 22:23:51 -0700

    start project IOS;2D

---
+++ ShowDevices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowDevices
+//
+//  Created by Nguyễn Lực on 7/19/19.
+//  Copyright © 2019 Nguyễn Lực. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
